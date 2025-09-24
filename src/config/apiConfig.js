4b4980427beb8244315357cecf922f3a0a6947a6
errishabhsrivastava <ersrivastavarishabh@gmail.com>
2025-09-24 08:23:35 +0530

    v0.0.1

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const apiConfig = {
-    baseURL: API_BASE_URL || 'http://localhost:5000/api/v1',
+    baseURL: API_BASE_URL || 'https://backend.shreevajewels.com/api/v1',
 };
 export { apiConfig };
